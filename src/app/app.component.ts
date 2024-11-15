641aadabc8b98098e181072fd4c554de0757fb7e
pranav <pranav.hs@sen-sei.in>
2024-11-15 12:28:26 +0530

    > Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  menuClicked = false;
   title = 'food-fiesta-app';
+  constructor(private router: Router) {}
+  route(link: string) {
+    this.router.navigate([link]);
+  }
 }
